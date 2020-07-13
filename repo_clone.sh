c6f40d937a90b43bbdc2a15f515273d7ba72f7b2
sumangala028 <sumangala028@gmail.com>
2020-07-13 08:19:49 +0530

    updated

---
+++ repo_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#/bin/bash
+
+repository = "git@github.com:ballerina-platform/ballerina-distribution.git"
+
+localFolder = "/ D:/WSO2_works/task2/githubAction/bbe-length-validation/build/cloned-repo"
+
+git clone "$repository" "$localFolder"
